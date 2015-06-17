7ec4fb4dd7d46dc7669409da4ab19c7134f896a8
ekelareva <ekelareva@ekelareva-macbookair.roam.corp.google.com>
2015-06-17 17:27:51 +1000

    Added scraper for Monash.

--- scraper.rb
+++ scraper.rb
@@ -1,25 +1,43 @@
-# This is a template for a Ruby scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+require 'scraperwiki'
+require 'mechanize'
 
-# require 'scraperwiki'
-# require 'mechanize'
-#
-# agent = Mechanize.new
-#
-# # Read in a page
-# page = agent.get("http://foo.com")
-#
-# # Find somehing on the page using css selectors
-# p page.at('div.content')
-#
-# # Write out to the sqlite database using scraperwiki library
-# ScraperWiki.save_sqlite(["name"], {"name" => "susan", "occupation" => "software developer"})
-#
-# # An arbitrary query against the database
-# ScraperWiki.select("* from data where 'name'='peter'")
+agent = Mechanize.new
 
-# You don't have to do things with the Mechanize or ScraperWiki libraries.
-# You can use whatever gems you want: https://morph.io/documentation/ruby
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+def scrape_page(page, url)
+  table = page.at("table.ContentPanel")
+  
+  table.search("tr")[1..-1].each do |tr|
+    day, month, year = tr.search("td")[3].inner_text.split("/").map{|s| s.to_i}
+    
+    record = {
+      "info_url" => url,
+      "comment_url" => url,
+      "council_reference" => tr.at("td a").inner_text,
+      "address" => tr.search("td")[1].inner_text,
+      "description" => tr.search("td")[2].inner_text,
+      "date_received" => Date.new(year, month, day).to_s,
+      "date_scraped" => Date.today.to_s
+    }
+    
+    # Check if record already exists
+    if (ScraperWiki.select("* from data where `council_reference`='#{record['council_reference']}'").empty? rescue true)
+      ScraperWiki.save_sqlite(['council_reference'], record)
+    else
+      puts "Skipping already saved record " + record['council_reference']
+    end
+  end
+end
+
+# Load summary page.
+url = "https://epathway.monash.vic.gov.au/ePathway/Production/Web/GeneralEnquiry/EnquiryLists.aspx?ModuleCode=LAP"
+page = agent.get(url)
+
+# Now do the paging magic
+number_pages =  page.at("#ctl00_MainBodyContent_mPagingControl_pageNumberLabel").inner_text.split(" ")[3].to_i
+
+(1..number_pages).each do |no|
+  url = "https://epathway.monash.vic.gov.au/ePathway/Production/Web/GeneralEnquiry/EnquirySummaryView.aspx?PageNumber=#{no}"
+  page = agent.get(url)
+  puts "Scraping page #{no} of " + number_pages.to_s + "..."
+  scrape_page(page, url)
+end
