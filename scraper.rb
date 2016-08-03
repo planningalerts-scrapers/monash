2c162edaa57220f19a655f011f324ec8c0cbcfed
JasonThomasData <jason.robert.thomas.123@gmail.com>
2016-08-03 18:51:50 +1000

    General refactor - db now has the correct fields that PA uses, less repitition

--- scraper.rb
+++ scraper.rb
@@ -5,14 +5,19 @@ agent = Mechanize.new
 
 def scrape_page(page)
   table = page.at("table.ContentPanel")
-  
-  table.search("tr")[1..-1].each do |tr|
+  tr_elems = table.search("tr")
+
+  tr_elems.each_with_index do |tr, index|
+    next if index == 0 #Skipping the first beacause it's got <th> not <td>
+
+    this_application_details = tr.css('td a')[0].attributes['href']
+    this_application_link = base_url + this_application_details.to_s
+
     day, month, year = tr.search("td")[3].inner_text.split("/").map{|s| s.to_i}
-    default_url = "https://epathway.monash.vic.gov.au/ePathway/Production/Web/GeneralEnquiry/EnquiryLists.aspx?ModuleCode=LAP"
-    
+
     record = {
-      "info_url" => default_url,
-      "comment_url" => default_url,
+      "info_url" => this_application_link,
+      "comment_url" => 'mailto:mail@monash.vic.gov.au',
       "council_reference" => tr.at("td a").inner_text,
       "address" => tr.search("td")[1].inner_text,
       "description" => tr.search("td")[2].inner_text,
@@ -43,8 +48,9 @@ page = form.submit(form.button_with(:value => "Next"))
 number_pages =  page.at("#ctl00_MainBodyContent_mPagingControl_pageNumberLabel").inner_text.split(" ")[3].to_i
 
 (1..number_pages).each do |no|
-  url = "https://epathway.monash.vic.gov.au/ePathway/Production/Web/GeneralEnquiry/EnquirySummaryView.aspx?PageNumber=#{no}"
-  page = agent.get(url)
+  result_page_extension = "/EnquirySummaryView.aspx?PageNumber=#{no}"
+  results_page_url = base_url + result_page_extension
+  page = agent.get(results_page_url)
   puts "Scraping page #{no} of " + number_pages.to_s + "..."
-  scrape_page(page)
+  scrape_page(page, base_url)
 end
