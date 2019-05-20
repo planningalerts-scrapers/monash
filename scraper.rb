a43c9285660ee699435cc1d6d53bc74b40d5456a
Matthew Landauer <mlandauer@gmail.com>
2019-05-21 05:30:49 +1000

    Use more of the library

--- scraper.rb
+++ scraper.rb
@@ -4,20 +4,6 @@ scraper = EpathwayScraper::Scraper.new(
   "https://epathway.monash.vic.gov.au/ePathway/Production"
 )
 
-agent = scraper.agent
-
-base_url = "https://epathway.monash.vic.gov.au/ePathway/Production/Web/GeneralEnquiry/"
-
-page = scraper.pick_type_of_search(:advertising)
-
-# Now do the paging magic
-number_pages = scraper.extract_total_number_of_pages(page)
-
-(1..number_pages).each do |no|
-  results_page_url = base_url + "EnquirySummaryView.aspx?PageNumber=#{no}"
-  page = agent.get(results_page_url)
-  puts "Scraping page #{no} of " + number_pages.to_s + "..."
-  scraper.scrape_index_page(page) do |record|
-    EpathwayScraper.save(record)
-  end
+scraper.scrape(list_type: :advertising, with_gets: true) do |record|
+  EpathwayScraper.save(record)
 end
