1f9532da1e3f420490a5163c106e4244f1625741
Matthew Landauer <mlandauer@gmail.com>
2019-05-21 05:14:14 +1000

    Use a url that doesn't require a session for info_url

--- scraper.rb
+++ scraper.rb
@@ -1,19 +1,16 @@
 require "epathway_scraper"
 
-def scrape_page(page, base_url)
+def scrape_page(page, base_url, scraper)
   table = page.at("table.ContentPanel")
   tr_elems = table.search("tr")
 
   tr_elems.each_with_index do |tr, index|
     next if index == 0 #Skipping the first beacause it's got <th> not <td>
 
-    this_application_details = tr.css('td a')[0].attributes['href']
-    this_application_link = base_url + this_application_details.to_s
-
     day, month, year = tr.search("td")[3].inner_text.split("/").map{|s| s.to_i}
 
     record = {
-      "info_url" => this_application_link,
+      "info_url" => scraper.base_url,
       "council_reference" => tr.at("td a").inner_text,
       "address" => tr.search("td")[1].inner_text,
       "description" => tr.search("td")[2].inner_text,
@@ -44,5 +41,5 @@ number_pages =  page.at("#ctl00_MainBodyContent_mPagingControl_pageNumberLabel")
   results_page_url = base_url + result_page_extension
   page = agent.get(results_page_url)
   puts "Scraping page #{no} of " + number_pages.to_s + "..."
-  scrape_page(page, base_url)
+  scrape_page(page, base_url, scraper)
 end
